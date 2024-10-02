5de55c9108743f8b66d4c4c52b466ed67cd4c2f2
joaopdadv <joaopdadv@gmail.com>
2024-10-02 09:37:41 -0300

    modules

---
+++ modules/activation_functions.py
@@ -0,0 +1,3 @@
+class Sigmoidal():
+    def __init__(self):
+        pass
---
+++ modules/structures.py
@@ -0,0 +1,7 @@
+class Layer():
+    def __init__(self) -> None:
+        pass
+
+class Network():
+    def __init__(self) -> None:
+        pass
